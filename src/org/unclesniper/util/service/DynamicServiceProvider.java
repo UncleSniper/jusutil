33b86da8b97f62150e7b10ca934120afdedc1f73
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2021-12-16 00:47:02 +0100

    Added UpperBoundT to DynamicServiceProvider

--- src/org/unclesniper/util/service/DynamicServiceProvider.java
+++ src/org/unclesniper/util/service/DynamicServiceProvider.java
@@ -2,11 +2,12 @@ package org.unclesniper.util.service;
 
 import static org.unclesniper.util.ArgUtils.notNull;
 
-public interface DynamicServiceProvider<ProvisionExceptionT extends Throwable> {
+public interface DynamicServiceProvider<UpperBoundT, ProvisionExceptionT extends Throwable> {
 
-	<ServiceT> ServiceT getService(Class<ServiceT> type) throws ProvisionExceptionT;
+	<ServiceT extends UpperBoundT> ServiceT getService(Class<ServiceT> type) throws ProvisionExceptionT;
 
-	default <ServiceT> ServiceT requireService(Class<ServiceT> type) throws ProvisionExceptionT {
+	default <ServiceT extends UpperBoundT> ServiceT requireService(Class<ServiceT> type)
+			throws ProvisionExceptionT {
 		ServiceT service = this.getService(type);
 		if(service == null)
 			throw new IllegalStateException("Don't know how to provide service of type '"
