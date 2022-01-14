bfc4124e519d6d98a2127653fcb9b349b6a94480
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2022-01-15 00:53:03 +0100

    Added ArgUtils.{length,end}Range{Ex,In}clusive()

--- src/org/unclesniper/util/ArgUtils.java
+++ src/org/unclesniper/util/ArgUtils.java
@@ -292,4 +292,372 @@ public final class ArgUtils {
 		return arg;
 	}
 
+	public static void lengthRangeExclusive(int offset, int length, int upperBound,
+			String offsetName, String lengthName) {
+		if(offsetName == null)
+			throw new IllegalArgumentException("Argument 'offsetName' to ArgUtils.lengthRangeExclusive() "
+					+ "must not be null");
+		if(lengthName == null)
+			throw new IllegalArgumentException("Argument 'lengthName' to ArgUtils.lengthRangeExclusive() "
+					+ "must not be null");
+		if(offset < 0)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " < 0");
+		if(offset >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " >= " + upperBound);
+		if(length < 0)
+			throw new IndexOutOfBoundsException("Argument '" + lengthName + "' is out of bounds: "
+					+ length + " < 0");
+		if(offset + length >= upperBound)
+			throw new IndexOutOfBoundsException("End of range '" + offsetName + "..+" + lengthName
+					+ "' is out of bounds: " + offset + " + " + length + " >= " + upperBound);
+	}
+
+	public static void lengthRangeInclusive(int offset, int length, int upperBound,
+			String offsetName, String lengthName) {
+		if(offsetName == null)
+			throw new IllegalArgumentException("Argument 'offsetName' to ArgUtils.lengthRangeInclusive() "
+					+ "must not be null");
+		if(lengthName == null)
+			throw new IllegalArgumentException("Argument 'lengthName' to ArgUtils.lengthRangeInclusive() "
+					+ "must not be null");
+		if(offset < 0)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " < 0");
+		if(offset > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " > " + upperBound);
+		if(length < 0)
+			throw new IndexOutOfBoundsException("Argument '" + lengthName + "' is out of bounds: "
+					+ length + " < 0");
+		if(offset + length > upperBound)
+			throw new IndexOutOfBoundsException("End of range '" + offsetName + "...+" + lengthName
+					+ "' is out of bounds: " + offset + " + " + length + " > " + upperBound);
+	}
+
+	public static void endRangeExclusive(int start, int end, int upperBound, String startName, String endName) {
+		if(startName == null)
+			throw new IllegalArgumentException("Argument 'startName' to ArgUtils.endRangeExclusive() "
+					+ "must not be null");
+		if(endName == null)
+			throw new IllegalArgumentException("Argument 'endName' to ArgUtils.endRangeExclusive() "
+					+ "must not be null");
+		if(start < 0)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " < 0");
+		if(start >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " >= " + upperBound);
+		if(end < 0)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds : "
+					+ end + " < 0");
+		if(end >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds: "
+					+ end + " >= " + upperBound);
+		if(end < start)
+			throw new IndexOutOfBoundsException("Range '" + startName + ".." + endName + "' has negative size: "
+					+ start + " > " + end);
+	}
+
+	public static void endRangeInclusive(int start, int end, int upperBound, String startName, String endName) {
+		if(startName == null)
+			throw new IllegalArgumentException("Argument 'startName' to ArgUtils.endRangeInclusive() "
+					+ "must not be null");
+		if(endName == null)
+			throw new IllegalArgumentException("Argument 'endName' to ArgUtils.endRangeInclusive() "
+					+ "must not be null");
+		if(start < 0)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " < 0");
+		if(start > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " > " + upperBound);
+		if(end < 0)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds : "
+					+ end + " < 0");
+		if(end > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds: "
+					+ end + " > " + upperBound);
+		if(end < start)
+			throw new IndexOutOfBoundsException("Range '" + startName + ".." + endName + "' has negative size: "
+					+ start + " > " + end);
+	}
+
+	public static void lengthRangeExclusive(short offset, short length, short upperBound,
+			String offsetName, String lengthName) {
+		if(offsetName == null)
+			throw new IllegalArgumentException("Argument 'offsetName' to ArgUtils.lengthRangeExclusive() "
+					+ "must not be null");
+		if(lengthName == null)
+			throw new IllegalArgumentException("Argument 'lengthName' to ArgUtils.lengthRangeExclusive() "
+					+ "must not be null");
+		if(offset < 0)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " < 0");
+		if(offset >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " >= " + upperBound);
+		if(length < 0)
+			throw new IndexOutOfBoundsException("Argument '" + lengthName + "' is out of bounds: "
+					+ length + " < 0");
+		if(offset + length >= upperBound)
+			throw new IndexOutOfBoundsException("End of range '" + offsetName + "..+" + lengthName
+					+ "' is out of bounds: " + offset + " + " + length + " >= " + upperBound);
+	}
+
+	public static void lengthRangeInclusive(short offset, short length, short upperBound,
+			String offsetName, String lengthName) {
+		if(offsetName == null)
+			throw new IllegalArgumentException("Argument 'offsetName' to ArgUtils.lengthRangeInclusive() "
+					+ "must not be null");
+		if(lengthName == null)
+			throw new IllegalArgumentException("Argument 'lengthName' to ArgUtils.lengthRangeInclusive() "
+					+ "must not be null");
+		if(offset < 0)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " < 0");
+		if(offset > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " > " + upperBound);
+		if(length < 0)
+			throw new IndexOutOfBoundsException("Argument '" + lengthName + "' is out of bounds: "
+					+ length + " < 0");
+		if(offset + length > upperBound)
+			throw new IndexOutOfBoundsException("End of range '" + offsetName + "...+" + lengthName
+					+ "' is out of bounds: " + offset + " + " + length + " > " + upperBound);
+	}
+
+	public static void endRangeExclusive(short start, short end, short upperBound, String startName, String endName) {
+		if(startName == null)
+			throw new IllegalArgumentException("Argument 'startName' to ArgUtils.endRangeExclusive() "
+					+ "must not be null");
+		if(endName == null)
+			throw new IllegalArgumentException("Argument 'endName' to ArgUtils.endRangeExclusive() "
+					+ "must not be null");
+		if(start < 0)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " < 0");
+		if(start >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " >= " + upperBound);
+		if(end < 0)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds : "
+					+ end + " < 0");
+		if(end >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds: "
+					+ end + " >= " + upperBound);
+		if(end < start)
+			throw new IndexOutOfBoundsException("Range '" + startName + ".." + endName + "' has negative size: "
+					+ start + " > " + end);
+	}
+
+	public static void endRangeInclusive(short start, short end, short upperBound, String startName, String endName) {
+		if(startName == null)
+			throw new IllegalArgumentException("Argument 'startName' to ArgUtils.endRangeInclusive() "
+					+ "must not be null");
+		if(endName == null)
+			throw new IllegalArgumentException("Argument 'endName' to ArgUtils.endRangeInclusive() "
+					+ "must not be null");
+		if(start < 0)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " < 0");
+		if(start > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " > " + upperBound);
+		if(end < 0)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds : "
+					+ end + " < 0");
+		if(end > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds: "
+					+ end + " > " + upperBound);
+		if(end < start)
+			throw new IndexOutOfBoundsException("Range '" + startName + ".." + endName + "' has negative size: "
+					+ start + " > " + end);
+	}
+
+	public static void lengthRangeExclusive(byte offset, byte length, byte upperBound,
+			String offsetName, String lengthName) {
+		if(offsetName == null)
+			throw new IllegalArgumentException("Argument 'offsetName' to ArgUtils.lengthRangeExclusive() "
+					+ "must not be null");
+		if(lengthName == null)
+			throw new IllegalArgumentException("Argument 'lengthName' to ArgUtils.lengthRangeExclusive() "
+					+ "must not be null");
+		if(offset < 0)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " < 0");
+		if(offset >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " >= " + upperBound);
+		if(length < 0)
+			throw new IndexOutOfBoundsException("Argument '" + lengthName + "' is out of bounds: "
+					+ length + " < 0");
+		if(offset + length >= upperBound)
+			throw new IndexOutOfBoundsException("End of range '" + offsetName + "..+" + lengthName
+					+ "' is out of bounds: " + offset + " + " + length + " >= " + upperBound);
+	}
+
+	public static void lengthRangeInclusive(byte offset, byte length, byte upperBound,
+			String offsetName, String lengthName) {
+		if(offsetName == null)
+			throw new IllegalArgumentException("Argument 'offsetName' to ArgUtils.lengthRangeInclusive() "
+					+ "must not be null");
+		if(lengthName == null)
+			throw new IllegalArgumentException("Argument 'lengthName' to ArgUtils.lengthRangeInclusive() "
+					+ "must not be null");
+		if(offset < 0)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " < 0");
+		if(offset > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " > " + upperBound);
+		if(length < 0)
+			throw new IndexOutOfBoundsException("Argument '" + lengthName + "' is out of bounds: "
+					+ length + " < 0");
+		if(offset + length > upperBound)
+			throw new IndexOutOfBoundsException("End of range '" + offsetName + "...+" + lengthName
+					+ "' is out of bounds: " + offset + " + " + length + " > " + upperBound);
+	}
+
+	public static void endRangeExclusive(byte start, byte end, byte upperBound, String startName, String endName) {
+		if(startName == null)
+			throw new IllegalArgumentException("Argument 'startName' to ArgUtils.endRangeExclusive() "
+					+ "must not be null");
+		if(endName == null)
+			throw new IllegalArgumentException("Argument 'endName' to ArgUtils.endRangeExclusive() "
+					+ "must not be null");
+		if(start < 0)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " < 0");
+		if(start >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " >= " + upperBound);
+		if(end < 0)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds : "
+					+ end + " < 0");
+		if(end >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds: "
+					+ end + " >= " + upperBound);
+		if(end < start)
+			throw new IndexOutOfBoundsException("Range '" + startName + ".." + endName + "' has negative size: "
+					+ start + " > " + end);
+	}
+
+	public static void endRangeInclusive(byte start, byte end, byte upperBound, String startName, String endName) {
+		if(startName == null)
+			throw new IllegalArgumentException("Argument 'startName' to ArgUtils.endRangeInclusive() "
+					+ "must not be null");
+		if(endName == null)
+			throw new IllegalArgumentException("Argument 'endName' to ArgUtils.endRangeInclusive() "
+					+ "must not be null");
+		if(start < 0)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " < 0");
+		if(start > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " > " + upperBound);
+		if(end < 0)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds : "
+					+ end + " < 0");
+		if(end > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds: "
+					+ end + " > " + upperBound);
+		if(end < start)
+			throw new IndexOutOfBoundsException("Range '" + startName + ".." + endName + "' has negative size: "
+					+ start + " > " + end);
+	}
+
+	public static void lengthRangeExclusive(long offset, long length, long upperBound,
+			String offsetName, String lengthName) {
+		if(offsetName == null)
+			throw new IllegalArgumentException("Argument 'offsetName' to ArgUtils.lengthRangeExclusive() "
+					+ "must not be null");
+		if(lengthName == null)
+			throw new IllegalArgumentException("Argument 'lengthName' to ArgUtils.lengthRangeExclusive() "
+					+ "must not be null");
+		if(offset < 0)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " < 0");
+		if(offset >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " >= " + upperBound);
+		if(length < 0)
+			throw new IndexOutOfBoundsException("Argument '" + lengthName + "' is out of bounds: "
+					+ length + " < 0");
+		if(offset + length >= upperBound)
+			throw new IndexOutOfBoundsException("End of range '" + offsetName + "..+" + lengthName
+					+ "' is out of bounds: " + offset + " + " + length + " >= " + upperBound);
+	}
+
+	public static void lengthRangeInclusive(long offset, long length, long upperBound,
+			String offsetName, String lengthName) {
+		if(offsetName == null)
+			throw new IllegalArgumentException("Argument 'offsetName' to ArgUtils.lengthRangeInclusive() "
+					+ "must not be null");
+		if(lengthName == null)
+			throw new IllegalArgumentException("Argument 'lengthName' to ArgUtils.lengthRangeInclusive() "
+					+ "must not be null");
+		if(offset < 0)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " < 0");
+		if(offset > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + offsetName + "' is out of bounds: "
+					+ offset + " > " + upperBound);
+		if(length < 0)
+			throw new IndexOutOfBoundsException("Argument '" + lengthName + "' is out of bounds: "
+					+ length + " < 0");
+		if(offset + length > upperBound)
+			throw new IndexOutOfBoundsException("End of range '" + offsetName + "...+" + lengthName
+					+ "' is out of bounds: " + offset + " + " + length + " > " + upperBound);
+	}
+
+	public static void endRangeExclusive(long start, long end, long upperBound, String startName, String endName) {
+		if(startName == null)
+			throw new IllegalArgumentException("Argument 'startName' to ArgUtils.endRangeExclusive() "
+					+ "must not be null");
+		if(endName == null)
+			throw new IllegalArgumentException("Argument 'endName' to ArgUtils.endRangeExclusive() "
+					+ "must not be null");
+		if(start < 0)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " < 0");
+		if(start >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " >= " + upperBound);
+		if(end < 0)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds : "
+					+ end + " < 0");
+		if(end >= upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds: "
+					+ end + " >= " + upperBound);
+		if(end < start)
+			throw new IndexOutOfBoundsException("Range '" + startName + ".." + endName + "' has negative size: "
+					+ start + " > " + end);
+	}
+
+	public static void endRangeInclusive(long start, long end, long upperBound, String startName, String endName) {
+		if(startName == null)
+			throw new IllegalArgumentException("Argument 'startName' to ArgUtils.endRangeInclusive() "
+					+ "must not be null");
+		if(endName == null)
+			throw new IllegalArgumentException("Argument 'endName' to ArgUtils.endRangeInclusive() "
+					+ "must not be null");
+		if(start < 0)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " < 0");
+		if(start > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + startName + "' is out of bounds: "
+					+ start + " > " + upperBound);
+		if(end < 0)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds : "
+					+ end + " < 0");
+		if(end > upperBound)
+			throw new IndexOutOfBoundsException("Argument '" + endName + "' is out of bounds: "
+					+ end + " > " + upperBound);
+		if(end < start)
+			throw new IndexOutOfBoundsException("Range '" + startName + ".." + endName + "' has negative size: "
+					+ start + " > " + end);
+	}
+
 }
