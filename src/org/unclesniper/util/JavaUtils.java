29b77694ec6a08e84781e1328eccb845c0263108
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2022-01-15 03:55:06 +0100

    Renamed JavaUtils.is{Package,Qualified}Name(); added JavaUtils.splitQualifiedName()

--- src/org/unclesniper/util/JavaUtils.java
+++ src/org/unclesniper/util/JavaUtils.java
@@ -1,5 +1,7 @@
 package org.unclesniper.util;
 
+import java.util.function.Consumer;
+
 import static org.unclesniper.util.ArgUtils.notNull;
 import static org.unclesniper.util.ArgUtils.lengthRangeExclusive;
 
@@ -26,7 +28,7 @@ public final class JavaUtils {
 		return JavaUtils.isIdentifierInternal(notNull(name, "name"), 0, name.length());
 	}
 
-	public static boolean isPackageName(String name) {
+	public static boolean isQualifiedName(String name) {
 		int length = notNull(name, "name").length(), start = 0;
 		if(length == 0)
 			return false;
@@ -39,6 +41,25 @@ public final class JavaUtils {
 			start = pos + 1;
 		}
 		return JavaUtils.isIdentifierInternal(name, start, length - start);
+	}
+
+	public static void splitQualifiedName(String name, Consumer<String> sink) {
+		int length = notNull(name, "name").length(), start = 0;
+		notNull(sink, "sink");
+		if(length == 0)
+			throw new IllegalArgumentException("Illegal Java qualified name: " + name);
+		for(;;) {
+			int pos = name.indexOf('.', start);
+			if(pos < 0)
+				break;
+			if(!JavaUtils.isIdentifierInternal(name, start, pos - start))
+				throw new IllegalArgumentException("Illegal Java qualified name: " + name);
+			sink.accept(name.substring(start, pos));
+			start = pos + 1;
+		}
+		if(!JavaUtils.isIdentifierInternal(name, start, length - start))
+			throw new IllegalArgumentException("Illegal Java qualified name: " + name);
+		sink.accept(name.substring(start));
 	}
 
 }
