3c6a45e129587ddab6b77a304c1face9282eb81f
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2022-01-15 05:07:15 +0100

    Added PartialStringable.toString(PartialStringable)

--- src/org/unclesniper/util/PartialStringable.java
+++ src/org/unclesniper/util/PartialStringable.java
@@ -1,7 +1,15 @@
 package org.unclesniper.util;
 
+import static org.unclesniper.util.ArgUtils.notNull;
+
 public interface PartialStringable {
 
 	void toString(StringBuilder sink);
+
+	public static String toString(PartialStringable object) {
+		StringBuilder builder = new StringBuilder();
+		notNull(object, "object").toString(builder);
+		return builder.toString();
+	}
 
 }
