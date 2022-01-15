8314013c0a35f83a80edee5d409a9b89ab1f6e06
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2022-01-15 04:58:05 +0100

    Added JavaQualifiedName.toString() methods

--- src/org/unclesniper/util/JavaQualifiedName.java
+++ src/org/unclesniper/util/JavaQualifiedName.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.Collections;
 
+import static org.unclesniper.util.ArgUtils.notNull;
 import static org.unclesniper.util.collection.CollectionUtils.copy;
 
 public final class JavaQualifiedName {
@@ -29,6 +30,25 @@ public final class JavaQualifiedName {
 		if(parent.isEmpty())
 			throw new IllegalStateException("Cannot pop Java qualified-name: No segments would remain");
 		return new JavaQualifiedName(parent);
+	}
+
+	public void toString(StringBuilder sink) {
+		notNull(sink, "sink");
+		boolean first = true;
+		for(String segment : segments) {
+			if(first)
+				first = false;
+			else
+				sink.append('.');
+			sink.append(segment);
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		toString(builder);
+		return builder.toString();
 	}
 
 }
